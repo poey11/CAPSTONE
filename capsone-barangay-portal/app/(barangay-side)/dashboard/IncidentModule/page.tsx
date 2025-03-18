13eb8ac3cbf1f8571e2619434b2dc13e6c3b7c4e
Deerrickk <john_derick_endozo@dlsu.edu.ph>
2025-03-19 00:14:18 +0800

    Fixed All Departments Render

--- capsone-barangay-portal/app/(barangay-side)/dashboard/IncidentModule/page.tsx
+++ capsone-barangay-portal/app/(barangay-side)/dashboard/IncidentModule/page.tsx
@@ -41,16 +41,16 @@ export default function MainPageIncident() {
    
   
   return (
-    <main className="main-container">
-      <div className="section-1">
+    <main className="main-container-all-department">
+      <div className="section-1-all-department">
         <h1>All Departments</h1>
 
       </div>
 
-      <div className="section-2">
-        <input type="text" className="search-bar" placeholder="Enter Incident Case" />
+      <div className="section-2-all-department">
+        <input type="text" className="search-bar-all-department" placeholder="Enter Incident Case" />
       
-        <select className="featuredStatus" defaultValue="">
+        <select className="featuredStatus-all-department" defaultValue="">
           <option value="" disabled>Status</option>
           {statusOptions.map((Deparment) => (
             <option key={Deparment} value={Deparment}>{Deparment}</option>
@@ -58,7 +58,7 @@ export default function MainPageIncident() {
         </select>
         
 
-        <select className="featuredStatus" defaultValue="">
+        <select className="featuredStatus-all-department" defaultValue="">
           <option value="" disabled>Deparment</option>
           {departmentOptions.map((status) => (
             <option key={status} value={status}>{status}</option>
@@ -66,7 +66,7 @@ export default function MainPageIncident() {
         </select>
 
 
-        <select className="featuredStatus" defaultValue="">
+        <select className="featuredStatus-all-department" defaultValue="">
           <option value="" disabled>Show...</option>
           <option value="5">Show 5</option>
           <option value="10">Show 10</option>
@@ -74,14 +74,14 @@ export default function MainPageIncident() {
       </div>
 
     
-      <div className="titlesection">
-             <p className="title"> Most Recent Incidents</p>
+      <div className="titlesection-all-department">
+             <p className="title-all-department"> Most Recent Incidents</p>
         </div>
      
-      <div className="main-section">
+      <div className="main-section-all-department">
         
 
-      <div className="table-section">
+      <div className="table-section-all-department">
         <table>
           <thead>
             <tr>
@@ -101,15 +101,15 @@ export default function MainPageIncident() {
                 <td>{incident.dateFiled} {incident.timeFiled}</td>
                 <td>{incident.nature}</td>
                 <td>
-                    <span className={`status-badge ${incident.status.toLowerCase().replace(" ", "-")}`}>
+                    <span className={`status-badge-all-department ${incident.status.toLowerCase().replace(" ", "-")}`}>
                         {incident.status}
                     </span>
                 </td>   
                 <td>
-                  <div className="actions">
-                    <button className="action-view" onClick={() => handleView(incident.id)}>View</button>
-                    <button className="action-edit" onClick={()=>handleEdit(incident.id) }>Edit</button>
-                    <button className="action-delete" onClick={()=> handleDelete(incident.id)}>Delete</button>
+                  <div className="actions-all-department">
+                    <button className="action-view-all-department" onClick={() => handleView(incident.id)}>View</button>
+                    <button className="action-edit-all-department" onClick={()=>handleEdit(incident.id) }>Edit</button>
+                    <button className="action-delete-all-department" onClick={()=> handleDelete(incident.id)}>Delete</button>
                   </div>
                 </td>
               </tr>
@@ -123,10 +123,10 @@ export default function MainPageIncident() {
     
      
 
-      <div className="incidentmap-section">
+      <div className="incidentmap-section-all-department">
         
-      <div className="titlesection">
-             <p className="title"> Incident HeatMap</p>
+      <div className="titlesection-all-department">
+             <p className="title-all-department"> Incident HeatMap</p>
         </div>
         <Heatmap/>
       
