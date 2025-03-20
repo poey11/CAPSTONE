7fec41fb3def1f3c23434f0913cb9fd820c8262f
justanne <justine_anne_y_rosete@dlsu.edu.ph>
2025-03-20 23:11:29 +0800

    reson for reject

---
+++ capsone-barangay-portal/app/(barangay-side)/dashboard/ServicesModule/OnlineRequests/ReasonForReject/page.tsx
@@ -0,0 +1,113 @@
+"use client"
+
+import { useRouter, useSearchParams} from "next/navigation";
+import type { Metadata } from "next";
+import { useState } from "react";
+import "@/CSS/User&Roles/ReasonForRejection.css";
+
+
+const metadata:Metadata = { 
+    title: "Reason For Rejection for Barangay Side",
+    description: "Reason For Rejection for Barangay Side",
+  };
+
+
+  export default function reasonForRejection() {
+
+    const router = useRouter();
+
+
+    const [showSubmitPopup, setShowSubmitPopup] = useState(false); 
+    const [showPopup, setShowPopup] = useState(false);
+    const [popupMessage, setPopupMessage] = useState("");
+    const [rejectionReason, setRejectionReason] = useState("");
+
+
+    const handleBack = () => {
+        router.push("/dashboard/ServicesModule/OnlineRequests/View");
+    };
+
+    const handleSubmitClick = async () => {
+        setShowSubmitPopup(true);
+    }
+
+    const confirmSubmit = async () => {
+        
+
+        try {
+
+            setPopupMessage("Reason for Rejection submitted successfully!");
+            setShowPopup(true);
+
+            setTimeout(() => {
+                setShowPopup(false);
+                router.push("/dashboard/ServicesModule/OnlineRequests");
+            }, 3000);
+        } catch (error) {
+            console.error("Error updating rejection reason:", error);
+        }
+    };
+
+    return (
+
+        <main className="reasonforrejection-main-container">
+        <div className="reasonforrejection-section-1">
+            <h1>Reject Resident User</h1>
+        </div>
+
+        <div className="reasonforrejection-main-section">
+            <div className="reasonforrejection-main-section1">
+                <div className="reasonforrejection-main-section1-left">
+                    <button onClick={handleBack}>
+                        <img src="/images/left-arrow.png" alt="Left Arrow" className="back-btn" />
+                    </button>
+
+                    <h1>Reason For Rejection</h1>
+                </div>
+                <div className="action-btn-section">
+                    <button className="submit-btn" onClick={handleSubmitClick}>Submit</button>
+                </div>
+            </div>
+
+            <hr/>
+
+            <div className="main-fields-container">
+                <div className="fields-container">
+                    <div className="fields-section">
+                        <p>State the Reason for Rejection</p>
+                            <textarea 
+                                className="reason" 
+                                placeholder="Enter Description"
+                                rows={10}
+                            ></textarea>
+                    </div>
+                </div>
+
+            </div>
+            
+        </div>
+
+        {showSubmitPopup && (
+                        <div className="confirmation-popup-overlay">
+                            <div className="confirmation-popup">
+                                <p>Are you sure you want to submit?</p>
+                                <div className="yesno-container">
+                                    <button onClick={() => setShowSubmitPopup(false)} className="no-button">No</button>
+                                    <button onClick={confirmSubmit} className="yes-button">Yes</button> 
+                                </div> 
+                            </div>
+                        </div>
+        )}
+
+        {showPopup && (
+                <div className={`popup-overlay show`}>
+                    <div className="popup">
+                        <p>{popupMessage}</p>
+                    </div>
+                </div>
+        )}
+
+    </main>
+
+    );
+}
