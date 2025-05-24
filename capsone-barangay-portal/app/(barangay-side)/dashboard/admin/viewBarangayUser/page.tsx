e0268d62906a98018a18fe96414888baa14175e7
Deerrickk <john_derick_endozo@dlsu.edu.ph>
2025-05-24 16:27:53 +0800

    First Part

--- capsone-barangay-portal/app/(barangay-side)/dashboard/admin/viewBarangayUser/page.tsx
+++ capsone-barangay-portal/app/(barangay-side)/dashboard/admin/viewBarangayUser/page.tsx
@@ -14,6 +14,7 @@ export default function ViewUser() {
 
     const [BarangayUserData, setBarangayUserData] = useState<any>(null);
     const [loading, setLoading] = useState(true);
+    const [activeSection, setActiveSection] = useState("basic");
 
     useEffect(() => {
         if (!barangayUserId) return;
@@ -65,51 +66,131 @@ export default function ViewUser() {
         window.location.href = "/dashboard/admin/BarangayUsers";
     };
 
+    
+
     return (
 
-        <main className="viewresident-main-container">
-       
-        {/*
-            <div className="path-section">
-                <h1 className="breadcrumb">User and Roles<span className="chevron">/</span></h1>
-                <h1 className="breadcrumb">
-                    <Link href="/dashboard/admin/BarangayUsers">Barangay Users</Link>
-                    <span className="chevron">/</span>
-                </h1>
-                <h2 className="breadcrumb">View Barangay User Details<span className="chevron"></span></h2>
-            </div>
-        
-            <div className="viewresident-page-title-section-1">
-                <h1>Barangay Users</h1>
-            </div>
-                */}
+        <main className="view-user-main-container">
+            
+            <div className="view-user-main-content">
 
-
-            <div className="viewresident-main-content">
-
-                <div className="viewresident-section-1">
-                    <div className="viewresident-main-section1-left">
-                        <button onClick={handleBack}>
-                            <img src="/images/left-arrow.png" alt="Left Arrow" className="back-btn"/> 
-                        </button>
-                        <h1>Barangay User Details</h1>
-
-                    </div>
+                    <div className="view-user-main-section1">
+                <div className="view-user-header-first-section">
+                    <img src="/Images/QClogo.png" alt="QC Logo" className="user-logo1-image-side-bar-1" />
                 </div>
 
-                {barangayUserFields.map((field) => (
-                    <div className="viewresident-details-section" key={field.key}>
-                        <div className="viewresident-title">
-                            <p>{field.label}</p>
-                        </div>
-                        <div className={`viewresident-description ${field.key === "residentNumber" ? "disabled-field" : ""}`}>
-                            <p> {BarangayUserData[field.key] ?? "N/A"} </p>
-                        </div>    
+                <div className="view-user-header-second-section">
+                    <h2 className="gov-info">Republic of the Philippines</h2>
+                    <h2 className="gov-info">Quezon City</h2>
+                    <h1 className="barangay-name">BARANGAY FAIRVIEW</h1>
+                    <h2 className="address">Dahlia Avenue, Fairview Park, Quezon City</h2>
+                    <h2 className="contact">930-0040 / 428-9030</h2>
+                </div>
+
+                <div className="view-user-header-third-section">
+                    <img src="/Images/logo.png" alt="Brgy Logo" className="user-logo2-image-side-bar-1" />
+                </div>
+            </div>
+
+              <div className="view-user-header-body">
+
+                <div className="view-user-header-body-top-section">
+                    <div className="view-user-backbutton-container">
+                        <button onClick={handleBack}>
+                            <img src="/images/left-arrow.png" alt="Left Arrow" className="user-back-btn-resident"/> 
+                        </button>
                     </div>
-                ))}
+
+
+                <div className="view-user-info-toggle-wrapper">
+                    {["basic", "full", "others" , "history"].map((section) => (
+                    <button
+                        key={section}
+                        type="button"
+                        className={`user-info-toggle-btn ${activeSection === section ? "active" : ""}`}
+                        onClick={() => setActiveSection(section)}
+                    >
+                        {section === "basic" && "Basic Info"}
+                        {section === "full" && "Full Info"}
+                        {section === "others" && "Others"}
+                        {section === "history" && "History"}
+                    </button>
+                    ))}
+                </div>  
+                </div>
+
+
+                <div className="view-user-header-body-bottom-section">
+
+                <div className="user-photo-section">
+            <span className="user-details-label">Barangay User Details</span>
+
+            <div className="user-profile-container">
+
+                {/*
+              <img
+                  src={formData.identificationFileURL || "/Images/default-identificationpic.jpg"}
+                  alt="Resident"
+                  className={
+                    formData.identificationFileURL
+                      ? "resident-picture uploaded-picture"
+                      : "resident-picture default-picture"
+                  }
+              />
+              <div className="user-name-section">
+                  <h2>
+                    {formData.firstName || "N/A"} {formData.lastName || "N/A"}
+                  </h2>
+                </div>
+                */}
+            </div>
+          </div>
+
+
+          <div className="view-user-info-main-container">
+               <div className="view-user-info-container-scrollable">
+
+                    <div className="view-user-info-main-content">
+
+                    {activeSection === "basic" && (
+                    <>
+
+                    <div className="view-main-user-content-left-side">
+
+                    </div>
+
+                    <div className="view-main-user-content-right-side">
+
+                    </div>
+
+
+                    
+                        </>
+                    )}
+
+                    </div>
+
+               </div>
+
+          </div>
+
+
+
+
+
+                </div>
+
+          
+
+                </div>
 
             </div>
 
+          
+     
+
+
+        
         </main>
     );
 }
