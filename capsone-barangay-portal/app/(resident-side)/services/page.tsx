b1dad5822faea102f2b10eb4a15c0909a10f1c8d
Deerrickk <john_derick_endozo@dlsu.edu.ph>
2025-07-02 17:24:25 +0800

    layout

--- capsone-barangay-portal/app/(resident-side)/services/page.tsx
+++ capsone-barangay-portal/app/(resident-side)/services/page.tsx
@@ -28,109 +28,99 @@ export default function Services() {
   }
 
   return (
-    <main className="services-container">
+    <main className="services-container-document">
       <div className="headerpic-services">
         <p>SERVICES</p>
       </div>
 
       <div className="services-main-container">
-        <div className="documents-container">
-          <div className="documents-container-column">
+        <div className="documents-upper">
+          <div className="documents-container">
 
-            <div className="tooltip-wrapper">
-              <div
-                className={`documents-card ${isGuest && !isAllowedForGuest("Barangay Certificate") ? "disabled-card" : ""}`}
-                onClick={goToServices}
-                id="Barangay Certificate"
-              >
-                <img src="/images/document.png" alt="Document Icon" className="document-icon" />
-                <h1>Barangay Certificate</h1>
-              </div>
-              {isGuest && !isAllowedForGuest("Barangay Certificate") && (
-                <span className="tooltip-text">Login required to request this document</span>
-              )}
-            </div>
+              
+               <div className="documents-container-column">
 
-            <div className="tooltip-wrapper">
-              <div
-                className={`documents-card ${isGuest && !isAllowedForGuest("Barangay Indigency") ? "disabled-card" : ""}`}
-                onClick={goToServices}
-                id="Barangay Indigency"
-              >
-                <img src="/images/document.png" alt="Document Icon" className="document-icon" />
-                <h1>Barangay Indigency</h1>
-              </div>
-              {isGuest && !isAllowedForGuest("Barangay Indigency") && (
-                <span className="tooltip-text">Login required to request this document</span>
-              )}
-            </div>
+                     <div className="tooltip-wrapper">
+                        <div
+                          className={`documents-card ${isGuest && !isAllowedForGuest("Barangay Certificate") ? "disabled-card" : ""}`}
+                          onClick={goToServices}
+                          id="Barangay Certificate"
+                        >
+                          <img src="/images/document.png" alt="Document Icon" className="document-icon" />
+                          <h1>Barangay Certificate</h1>
+                        </div>
+                        {isGuest && !isAllowedForGuest("Barangay Certificate") && (
+                          <span className="tooltip-text">Login required to request this document</span>
+                        )}
+                      </div>
 
-            <div className="tooltip-wrapper">
-              <div
-                className={`documents-card ${isGuest && !isAllowedForGuest("Barangay ID") ? "disabled-card" : ""}`}
-                onClick={goToServices}
-                id="Barangay ID"
-              >
-                <img src="/images/document.png" alt="Document Icon" className="document-icon" />
-                <h1>Barangay ID</h1>
+                      <div className="tooltip-wrapper">
+                        <div
+                          className={`documents-card ${isGuest && !isAllowedForGuest("Barangay Indigency") ? "disabled-card" : ""}`}
+                          onClick={goToServices}
+                          id="Barangay Indigency"
+                        >
+                          <img src="/images/document.png" alt="Document Icon" className="document-icon" />
+                          <h1>Barangay Indigency</h1>
+                        </div>
+                        {isGuest && !isAllowedForGuest("Barangay Indigency") && (
+                          <span className="tooltip-text">Login required to request this document</span>
+                        )}
+                  </div>
+
+                
+               </div>
+
+
+               <div className="documents-container-column">
+
+                     <div className="documents-card dropdown-container">
+                      <img src="/images/document.png" alt="Document Icon" className="document-icon" />
+                      <h1>Barangay Permits</h1>
+                      <div className="dropdown">
+                        <p id="Temporary Business Permit" onClick={goToServices}>Temporary Business Permit</p>
+                        <p id="Business Permit" onClick={goToServices}>Business Permit</p>
+                        <p id="Construction Permit" onClick={goToServices}>Construction Permit</p>
+                      </div>
+                    </div>
+
+    
+                      <div
+                        className={`documents-card ${isGuest && !isAllowedForGuest("Barangay Clearance") ? "disabled-card" : ""}`}
+                        onClick={goToServices}
+                        id="Barangay Clearance"
+                      >
+                        <img src="/images/document.png" alt="Document Icon" className="document-icon" />
+                        <h1>Barangay Clearance</h1>
+                      </div>
+                      {isGuest && !isAllowedForGuest("Barangay Clearance") && (
+                        <span className="tooltip-text">Login required to request this document</span>
+                      )}
+
+               </div>
+
+                
+
+
+              
               </div>
-              {isGuest && !isAllowedForGuest("Barangay ID") && (
-                <span className="tooltip-text">Login required to request this document</span>
-              )}
-            </div>
+
           </div>
 
-          <div className="documents-container-column">
+          <div className="documents-lower">
 
-            <div className="documents-card dropdown-container">
-              <img src="/images/document.png" alt="Document Icon" className="document-icon" />
-              <h1>Barangay Permits</h1>
-              <div className="dropdown">
-                <p id="Temporary Business Permit" onClick={goToServices}>Temporary Business Permit</p>
-                <p id="Business Permit" onClick={goToServices}>Business Permit</p>
-                <p id="Construction Permit" onClick={goToServices}>Construction Permit</p>
+                <div className="documents-container-column-other">
+               
+                    <div className="documents-card" onClick={gotoOtherDocuments}>
+                      <img src="/images/document.png" alt="Document Icon" className="document-icon" />
+                      <h1>Other Documents</h1>
+                    </div>  
+                  
               </div>
-            </div>
 
-            <div className="tooltip-wrapper">
-              <div
-                className={`documents-card ${isGuest && !isAllowedForGuest("Barangay Clearance") ? "disabled-card" : ""}`}
-                onClick={goToServices}
-                id="Barangay Clearance"
-              >
-                <img src="/images/document.png" alt="Document Icon" className="document-icon" />
-                <h1>Barangay Clearance</h1>
-              </div>
-              {isGuest && !isAllowedForGuest("Barangay Clearance") && (
-                <span className="tooltip-text">Login required to request this document</span>
-              )}
-            </div>
-
-            <div className="tooltip-wrapper">
-              <div
-                className={`documents-card ${isGuest && !isAllowedForGuest("First Time Jobseeker") ? "disabled-card" : ""}`}
-                onClick={goToServices}
-                id="First Time Jobseeker"
-              >
-                <img src="/images/document.png" alt="Document Icon" className="document-icon" />
-                <h1>First Time Jobseeker</h1>
-              </div>
-              {isGuest && !isAllowedForGuest("First Time Jobseeker") && (
-                <span className="tooltip-text">Login required to request this document</span>
-              )}
-            </div>
-
-          </div>
-          <div className="documents-container-column">
-              <div className="tooltip-wrapper">
-                <div className="documents-card" onClick={gotoOtherDocuments}>
-                  <img src="/images/document.png" alt="Document Icon" className="document-icon" />
-                  <h1>Other Documents</h1>
-                </div>  
-              </div>
           </div>
         </div>
-      </div>
+   
     </main>
   );
 }
