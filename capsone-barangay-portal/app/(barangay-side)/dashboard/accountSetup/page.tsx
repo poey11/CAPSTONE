7a0ea83a7a5b3195afbb702dbbb13348fbbf5acf
poey11 <mocpayao15@gmail.com>
2025-07-05 04:06:13 +0800

    Refactor AccountSetupPage to restore and uncomment searchParams type definition for improved type safety

--- capsone-barangay-portal/app/(barangay-side)/dashboard/accountSetup/page.tsx
+++ capsone-barangay-portal/app/(barangay-side)/dashboard/accountSetup/page.tsx
@@ -6,45 +6,14 @@ import { doc, collection, getDoc } from "firebase/firestore";
 import { db } from "@/app/db/firebase";
 import Form from "@/app/(barangay-side)/components/accSetupForm";
 
-// @ts-ignore
-// interface AccountSetupPageProps {
-//   // searchParams: { [key: string]: string | string[] | undefined } | Promise<{ [key: string]: string | string[] | undefined }>;
-//   searchParams: { [key: string]: string | string[] | undefined };
-// }
-// @ts-ignore
-// export default async function AccountSetupPage({searchParams}: AccountSetupPageProps) {
-//   const returnUrl = (searchParams.returnUrl as string) || "/dashboard";
-//   const session = await getServerSession(authOptions);
-//   if (!session) {
-//     redirect("/");
-//     return null;
-//   }
-
-//   const userId = session.user.id;
-//   const userDocRef = doc(collection(db, "BarangayUsers"), userId);
-//   const userDoc = await getDoc(userDocRef);
-//   const userData = userDoc.data();
-
-//   if (!userData?.firstTimelogin) {
-//     console.log(returnUrl);
-//     redirect(returnUrl);
-//     return null;
-//   }
-//   return (
-  
-//       <Form userID={userId} />
-  
-//   );
-// }
+interface AccountSetupPageProps {
+  searchParams: { [key: string]: string | string[] | undefined } | Promise<{ [key: string]: string | string[] | undefined }>;
+}
 
 
-export default async function AccountSetupPage({
-  searchParams,
-}: {
-  searchParams: { [key: string]: string | string[] | undefined };
-}) {
-  const returnUrl = (searchParams.returnUrl as string) || "/dashboard";
-
+export default async function AccountSetupPage({searchParams}: AccountSetupPageProps) {
+  const params = await searchParams;
+  const returnUrl = (params.returnUrl as string) || "/dashboard";
   const session = await getServerSession(authOptions);
   if (!session) {
     redirect("/");
@@ -57,9 +26,13 @@ export default async function AccountSetupPage({
   const userData = userDoc.data();
 
   if (!userData?.firstTimelogin) {
+    console.log(returnUrl);
     redirect(returnUrl);
     return null;
   }
-
-  return <Form userID={userId} />;
+  return (
+  
+      <Form userID={userId} />
+  
+  );
 }
