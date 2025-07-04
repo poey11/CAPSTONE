95f2025ac0dd7010defcee2047e017d0e73dd7f7
poey11 <mocpayao15@gmail.com>
2025-07-05 04:03:03 +0800

    Refactor AccountSetupPage to clean up commented code and improve readability

--- capsone-barangay-portal/app/(barangay-side)/dashboard/accountSetup/page.tsx
+++ capsone-barangay-portal/app/(barangay-side)/dashboard/accountSetup/page.tsx
@@ -7,13 +7,44 @@ import { db } from "@/app/db/firebase";
 import Form from "@/app/(barangay-side)/components/accSetupForm";
 
 // @ts-ignore
-interface AccountSetupPageProps {
-  // searchParams: { [key: string]: string | string[] | undefined } | Promise<{ [key: string]: string | string[] | undefined }>;
-  searchParams: { [key: string]: string | string[] | undefined };
-}
+// interface AccountSetupPageProps {
+//   // searchParams: { [key: string]: string | string[] | undefined } | Promise<{ [key: string]: string | string[] | undefined }>;
+//   searchParams: { [key: string]: string | string[] | undefined };
+// }
 // @ts-ignore
-export default async function AccountSetupPage({searchParams}: AccountSetupPageProps) {
+// export default async function AccountSetupPage({searchParams}: AccountSetupPageProps) {
+//   const returnUrl = (searchParams.returnUrl as string) || "/dashboard";
+//   const session = await getServerSession(authOptions);
+//   if (!session) {
+//     redirect("/");
+//     return null;
+//   }
+
+//   const userId = session.user.id;
+//   const userDocRef = doc(collection(db, "BarangayUsers"), userId);
+//   const userDoc = await getDoc(userDocRef);
+//   const userData = userDoc.data();
+
+//   if (!userData?.firstTimelogin) {
+//     console.log(returnUrl);
+//     redirect(returnUrl);
+//     return null;
+//   }
+//   return (
+  
+//       <Form userID={userId} />
+  
+//   );
+// }
+
+
+export default async function AccountSetupPage({
+  searchParams,
+}: {
+  searchParams: { [key: string]: string | string[] | undefined };
+}) {
   const returnUrl = (searchParams.returnUrl as string) || "/dashboard";
+
   const session = await getServerSession(authOptions);
   if (!session) {
     redirect("/");
@@ -26,13 +57,9 @@ export default async function AccountSetupPage({searchParams}: AccountSetupPageP
   const userData = userDoc.data();
 
   if (!userData?.firstTimelogin) {
-    console.log(returnUrl);
     redirect(returnUrl);
     return null;
   }
-  return (
-  
-      <Form userID={userId} />
-  
-  );
+
+  return <Form userID={userId} />;
 }
