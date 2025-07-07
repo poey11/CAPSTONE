56810dbcd933cab401e33b80a537c7dcffe55e06
justanne <justine_anne_y_rosete@dlsu.edu.ph>
2025-07-07 15:59:26 +0800

    fixed sidemenu

--- capsone-barangay-portal/app/(barangay-side)/components/bMenu.tsx
+++ capsone-barangay-portal/app/(barangay-side)/components/bMenu.tsx
@@ -10,192 +10,192 @@ const BMenu: React.FC = () => {
 
   const [hoveredDropdown, setHoveredDropdown] = useState<string | null>(null);
   return (
-    <div className="side-bar">
-      <div className="logo-side-bar">
-        <img src="/Images/QClogo.png" alt="Barangay Captain" className="logo-image-side-bar" />
+    <div className="side-bar-brgyside">
+      <div className="logo-side-bar-brgyside">
+        <img src="/Images/QClogo.png" alt="Barangay Captain" className="logo-image-side-bar-brgyside" />
       </div>
 
-      <div className="contents-side-bar">
+      <div className="contents-side-bar-brgyside">
        
           {/* Dashboard Module */}
-      <div className="dropdown-wrapper"
+      <div className="dropdown-wrapper-brgyside"
           onMouseEnter={() => setHoveredDropdown("dashboard")}
           onMouseLeave={() => setHoveredDropdown(null)}
       >
 
-        <div className="dropdown-button">
+        <div className="dropdown-button-brgyside">
           <Link href="/dashboard">Dashboard</Link>
           <img 
               src={hoveredDropdown === "dashboard" ? "/images/left-arrow.png" : "/images/down-arrow.png"} 
               alt="Menu Icon" 
-              className="arrow" 
+              className="arrow-brgyside" 
             />
         </div>
-        <div className="dropdown-container">
-          <Link href="/dashboard/ReportsModule" className="dropdown-item-brgy">Generate Report</Link>
+        <div className="dropdown-container-brgyside">
+          <Link href="/dashboard/ReportsModule" className="dropdown-item-brgy-brgyside">Generate Report</Link>
         </div>
       </div>
 
         
         {/* User & Roles */}
-        <div className="dropdown-wrapper"
+        <div className="dropdown-wrapper-brgyside"
           onMouseEnter={() => setHoveredDropdown("user&roles")}
           onMouseLeave={() => setHoveredDropdown(null)}
         >
 
-          <div className="dropdown-button">
+          <div className="dropdown-button-brgyside">
             <Link href="/dashboard/admin/BarangayUsers">User and Roles</Link>
             <img 
                 src={hoveredDropdown === "user&roles" ? "/images/left-arrow.png" : "/images/down-arrow.png"} 
                 alt="Menu Icon" 
-                className="arrow" 
+                className="arrow-brgyside" 
               />
           </div>
 
-          <div className="dropdown-container">
-            <Link href="/dashboard/admin/BarangayUsers" className="dropdown-item-brgy">Barangay Users</Link>
-            <Link href="/dashboard/admin/PendingResidentUsers" className="dropdown-item-brgy">Pending Resident Users</Link>
-            <Link href="/dashboard/admin/ResidentUsers" className="dropdown-item-brgy">Resident Users</Link>
+          <div className="dropdown-container-brgyside">
+            <Link href="/dashboard/admin/BarangayUsers" className="dropdown-item-brgy-brgyside">Barangay Users</Link>
+            <Link href="/dashboard/admin/PendingResidentUsers" className="dropdown-item-brgy-brgyside">Pending Resident Users</Link>
+            <Link href="/dashboard/admin/ResidentUsers" className="dropdown-item-brgy-brgyside">Resident Users</Link>
             
 
           </div>
         </div>
 
         {/* Resident Management */}
-        <div className="dropdown-wrapper"
+        <div className="dropdown-wrapper-brgyside"
           onMouseEnter={() => setHoveredDropdown("resident")}
           onMouseLeave={() => setHoveredDropdown(null)}
         >
           
-        <div className="dropdown-button">
+        <div className="dropdown-button-brgyside">
           <Link href="/dashboard/ResidentModule">Residents Management</Link>
           <img 
               src={hoveredDropdown === "resident" ? "/images/left-arrow.png" : "/images/down-arrow.png"} 
               alt="Menu Icon" 
-              className="arrow" 
+              className="arrow-brgyside" 
             />
         </div>
 
 
-          <div className="dropdown-container">
-            <Link href="/dashboard/ResidentModule" className="dropdown-item-brgy">Main Residents</Link>
-            <Link href="/dashboard/ResidentModule/registeredVoters" className="dropdown-item-brgy">Registered Voters</Link>
-            <Link href="/dashboard/ResidentModule/kasambahayList" className="dropdown-item-brgy">Kasambahay Master List</Link>
-            <Link href="/dashboard/ResidentModule/FirstTimeJobSeeker" className="dropdown-item-brgy">First-Time Job Seekers List</Link>
+          <div className="dropdown-container-brgyside">
+            <Link href="/dashboard/ResidentModule" className="dropdown-item-brgy-brgyside">Main Residents</Link>
+            <Link href="/dashboard/ResidentModule/registeredVoters" className="dropdown-item-brgy-brgyside">Registered Voters</Link>
+            <Link href="/dashboard/ResidentModule/kasambahayList" className="dropdown-item-brgy-brgyside">Kasambahay Master List</Link>
+            <Link href="/dashboard/ResidentModule/FirstTimeJobSeeker" className="dropdown-item-brgy-brgyside">First-Time Job Seekers List</Link>
 
           </div>
         </div>
 
         {/* Officials Module */}
-        <div className="dropdown-wrapper"
+        <div className="dropdown-wrapper-brgyside"
           onMouseEnter={() => setHoveredDropdown("officials")}
           onMouseLeave={() => setHoveredDropdown(null)}
       >
           
-        <div className="dropdown-button">
+        <div className="dropdown-button-brgyside">
           <Link  href="/dashboard/OfficialsModule">Officials Module</Link>
           <img 
               src={hoveredDropdown === "officials" ? "/images/left-arrow.png" : "/images/down-arrow.png"} 
               alt="Menu Icon" 
-              className="arrow" 
+              className="arrow-brgyside" 
             />
         </div>
           
-          <div className="dropdown-container">
-            <Link href="/dashboard/OfficialsModule" className="dropdown-item-brgy">Officials</Link>
-            <Link href="/dashboard/OfficialsModule/SitioHoaOfficers" className="dropdown-item-brgy">Sitio/HOA Officers</Link>
+          <div className="dropdown-container-brgyside">
+            <Link href="/dashboard/OfficialsModule" className="dropdown-item-brgy-brgyside">Officials</Link>
+            <Link href="/dashboard/OfficialsModule/SitioHoaOfficers" className="dropdown-item-brgy-brgyside">Sitio/HOA Officers</Link>
           </div>
         </div>
 
 
 
         {/* Services Module */}
-        <div className="dropdown-wrapper"
+        <div className="dropdown-wrapper-brgyside"
           onMouseEnter={() => setHoveredDropdown("services")}
           onMouseLeave={() => setHoveredDropdown(null)}
       >
 
-        <div className="dropdown-button">
+        <div className="dropdown-button-brgyside">
           <Link href="/dashboard/ServicesModule/InBarangayRequests">Services Management</Link>
           <img 
               src={hoveredDropdown === "services" ? "/images/left-arrow.png" : "/images/down-arrow.png"} 
               alt="Menu Icon" 
-              className="arrow" 
+              className="arrow-brgyside" 
             />
         </div>
 
-          <div className="dropdown-container">
-            <Link href="/dashboard/ServicesModule/InBarangayRequests" className="dropdown-item-brgy">In Barangay Requests</Link>
-            <Link href="/dashboard/ServicesModule/OnlineRequests" className="dropdown-item-brgy">Online Requests</Link>
-            <Link href="/dashboard/ServicesModule/Appointments" className="dropdown-item-brgy">Appointments</Link>
+          <div className="dropdown-container-brgyside">
+            <Link href="/dashboard/ServicesModule/InBarangayRequests" className="dropdown-item-brgy-brgyside">In Barangay Requests</Link>
+            <Link href="/dashboard/ServicesModule/OnlineRequests" className="dropdown-item-brgy-brgyside">Online Requests</Link>
+            <Link href="/dashboard/ServicesModule/Appointments" className="dropdown-item-brgy-brgyside">Appointments</Link>
           </div>
         </div>
 
       {/* Incident Module */}
-      <div className="dropdown-wrapper"
+      <div className="dropdown-wrapper-brgyside"
           onMouseEnter={() => setHoveredDropdown("incidents")}
           onMouseLeave={() => setHoveredDropdown(null)}
       >
 
-        <div className="dropdown-button">
+        <div className="dropdown-button-brgyside">
           <Link href="/dashboard/IncidentModule">Incident Management</Link>
           <img 
               src={hoveredDropdown === "incidents" ? "/images/left-arrow.png" : "/images/down-arrow.png"} 
               alt="Menu Icon" 
-              className="arrow" 
+              className="arrow-brgyside" 
             />
         </div>
-        <div className="dropdown-container">
-          <Link href="/dashboard/IncidentModule/Department?id=Lupon" className="dropdown-item-brgy">Lupon</Link>
-          <Link href="/dashboard/IncidentModule/Department?id=GAD" className="dropdown-item-brgy">GAD</Link>
-          <Link href="/dashboard/IncidentModule/Department?id=BCPC" className="dropdown-item-brgy">BCPC</Link>
-          <Link href="/dashboard/IncidentModule/Department?id=VAWC" className="dropdown-item-brgy">VAWC</Link>
-          <Link href="/dashboard/IncidentModule/OnlineReports" className="dropdown-item-brgy">Online Reports</Link>
+        <div className="dropdown-container-brgyside">
+          <Link href="/dashboard/IncidentModule/Department?id=Lupon" className="dropdown-item-brgy-brgyside">Lupon</Link>
+          <Link href="/dashboard/IncidentModule/Department?id=GAD" className="dropdown-item-brgy-brgyside">GAD</Link>
+          <Link href="/dashboard/IncidentModule/Department?id=BCPC" className="dropdown-item-brgy-brgyside">BCPC</Link>
+          <Link href="/dashboard/IncidentModule/Department?id=VAWC" className="dropdown-item-brgy-brgyside">VAWC</Link>
+          <Link href="/dashboard/IncidentModule/OnlineReports" className="dropdown-item-brgy-brgyside">Online Reports</Link>
         </div>
       </div>
 
-        <Link href="/dashboard/announcements" className="module-title">Announcements</Link>
+        <Link href="/dashboard/announcements" className="module-title-brgyside">Announcements</Link>
 
 
 {/* For CAP2 */}
 
             {/* Programs and Events Module
-          <div className="dropdown-wrapper"
+          <div className="dropdown-wrapper-brgyside"
             onMouseEnter={() => setHoveredDropdown("programs")}
             onMouseLeave={() => setHoveredDropdown(null)}
         >
 
-            <div className="dropdown-button">
+            <div className="dropdown-button-brgyside">
               <Link href="/dashboard">Programs</Link>
               <img 
                   src={hoveredDropdown === "programs" ? "/images/left-arrow.png" : "/images/down-arrow.png"} 
                   alt="Menu Icon" 
-                  className="arrow" 
+                  className="arrow-brgyside" 
                 />
             </div>
-            <div className="dropdown-container">
-            <Link href="/dashboard/ProgramsModule" className="dropdown-item-brgy">Volunteer Programs</Link>
-            <Link href="/dashboard/ProgramsModule/ProgramResponses" className="dropdown-item-brgy">Program Responses</Link>
+            <div className="dropdown-container-brgyside">
+            <Link href="/dashboard/ProgramsModule" className="dropdown-item-brgy-brgyside">Volunteer Programs</Link>
+            <Link href="/dashboard/ProgramsModule/ProgramResponses" className="dropdown-item-brgy-brgyside">Program Responses</Link>
             </div>
         </div> */}
 
           {/* Events Module */}
-          {/* <div className="dropdown-wrapper"
+          {/* <div className="dropdown-wrapper-brgyside"
             onMouseEnter={() => setHoveredDropdown("events")}
             onMouseLeave={() => setHoveredDropdown(null)}
           >
 
-            <div className="dropdown-button">
+            <div className="dropdown-button-brgyside">
               <Link href="/dashboard">Events</Link>
               <img 
                   src={hoveredDropdown === "events" ? "/images/left-arrow.png" : "/images/down-arrow.png"} 
                   alt="Menu Icon" 
-                  className="arrow" 
+                  className="arrow-brgyside" 
                 />
             </div>
-            <div className="dropdown-container">
-            <Link href="/dashboard/EventsModule" className="dropdown-item-brgy">Events List</Link>
-            <Link href="/dashboard/EventsModule/EventResponses" className="dropdown-item-brgy">Event Responses</Link>
+            <div className="dropdown-container-brgyside">
+            <Link href="/dashboard/EventsModule" className="dropdown-item-brgy-brgyside">Events List</Link>
+            <Link href="/dashboard/EventsModule/EventResponses" className="dropdown-item-brgy-brgyside">Event Responses</Link>
             </div>
         </div>   */}
 
