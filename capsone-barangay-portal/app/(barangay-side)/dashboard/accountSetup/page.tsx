fe7118a300d1a5d26d13896fbb41f0f9cc7ba36b
poey11 <mocpayao15@gmail.com>
2025-08-13 22:10:46 +0800

    refactor: move authentication options to a separate file for better organization

--- capsone-barangay-portal/app/(barangay-side)/dashboard/accountSetup/page.tsx
+++ capsone-barangay-portal/app/(barangay-side)/dashboard/accountSetup/page.tsx
@@ -1,7 +1,7 @@
 // app/dashboard/accountSetup/page.tsx
 import { redirect } from "next/navigation";
 import { getServerSession } from "next-auth/next";
-import { authOptions } from "@/app/api/auth/[...nextauth]/route";
+import { authOptions } from "@/app/api/lib/auth";
 import { doc, collection, getDoc } from "firebase/firestore";
 import { db } from "@/app/db/firebase";
 import Form from "@/app/(barangay-side)/components/accSetupForm";
