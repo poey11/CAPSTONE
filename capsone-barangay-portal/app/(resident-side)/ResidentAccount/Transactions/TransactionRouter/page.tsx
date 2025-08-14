47cc3608d2fae45815e5e8aefb1ec5345c9ee703
poey11 <mocpayao15@gmail.com>
2025-08-15 05:08:27 +0800

    refactor: simplify TransactionRouter by removing unused imports and leveraging passed searchParams

--- capsone-barangay-portal/app/(resident-side)/ResidentAccount/Transactions/TransactionRouter/page.tsx
+++ capsone-barangay-portal/app/(resident-side)/ResidentAccount/Transactions/TransactionRouter/page.tsx
@@ -1,27 +1,21 @@
-"use client";
-import { useSearchParams } from "next/navigation";
-import { useEffect } from "react";
 import DocumentTransactionsDetails from "@/app/(resident-side)/ResidentAccount/Transactions/DocumentTransactions/page";
 import OtherDocumentTransactions from "@/app/(resident-side)/ResidentAccount/Transactions/DocumentTransactions/OtherDocumentTransactions/page";
 import IncidentTransactionsDetails from "@/app/(resident-side)/ResidentAccount/Transactions/IncidentTransactions/page";
 
-export default function TransactionRouter() {
-    const searchParams = useSearchParams();
-    const id = searchParams.get("id");
-    const type = searchParams.get("type");
+export default function TransactionRouter({
+  searchParams,
+}: {
+  searchParams: { id?: string; type?: string };
+}) {
+  const { id, type } = searchParams;
 
-    if (!id) return null;
+  if (!id) return null;
 
-    if (type === "ServiceRequest") {
-        return <DocumentTransactionsDetails referenceId={id} />;
-    }
-    if (type === "IncidentReport") {
-        return <IncidentTransactionsDetails referenceId={id} />;
-    }
-    return <OtherDocumentTransactions id={id} />;
-
-
-
-
-    return null; // This component redirects and does not render anything
+  if (type === "ServiceRequest") {
+    return <DocumentTransactionsDetails referenceId={id} />;
+  }
+  if (type === "IncidentReport") {
+    return <IncidentTransactionsDetails referenceId={id} />;
+  }
+  return <OtherDocumentTransactions id={id} />;
 }
