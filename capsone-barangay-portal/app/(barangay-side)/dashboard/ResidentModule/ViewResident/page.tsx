ac898537f3a417ea452cebe3e1a8d1663d19f213
poey11 <mocpayao15@gmail.com>
2025-08-20 02:52:22 +0800

    fix: Update image path casing for consistency in ViewResident component

--- capsone-barangay-portal/app/(barangay-side)/dashboard/ResidentModule/ViewResident/page.tsx
+++ capsone-barangay-portal/app/(barangay-side)/dashboard/ResidentModule/ViewResident/page.tsx
@@ -213,7 +213,7 @@ useEffect(() => {
     <div className="view-resident-main-content">
       <div className="view-resident-main-section1">
           <div className="view-resident-header-first-section">
-            <img src="/Images/QClogo.png" alt="QC Logo" className="logo1-image-side-bar-1" />
+            <img src="/Images/QCLogo.png" alt="QC Logo" className="logo1-image-side-bar-1" />
           </div>
 
           <div className="view-resident-header-second-section">
@@ -234,7 +234,7 @@ useEffect(() => {
         <div className="view-resident-header-body-top-section">
           <div className="view-resident-backbutton-container">
             <button onClick={handleBack}>
-              <img src="/images/left-arrow.png" alt="Left Arrow" className="back-btn-main-resident"/> 
+              <img src="/Images/left-arrow.png" alt="Left Arrow" className="back-btn-main-resident"/> 
             </button>
           </div>
               
--- capsone-barangay-portal/next.config.ts
+++ capsone-barangay-portal/next.config.ts
@@ -1,8 +1,11 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
-  
+  reactStrictMode: true,
+  swcMinify: true,
+  compiler: {
+    removeConsole: process.env.NODE_ENV === "production",
+  },
 };
 
 export default nextConfig;
