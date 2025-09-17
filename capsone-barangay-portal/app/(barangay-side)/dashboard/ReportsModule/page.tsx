18a5ee4215349ddf6cf53b1c0c3fa158ef2f68b6
Gujetamaa <jerico.ibanez18@gmail.com>
2025-09-18 00:14:33 +0800

    added rtga and dismissed statuses

--- capsone-barangay-portal/app/(barangay-side)/dashboard/ReportsModule/page.tsx
+++ capsone-barangay-portal/app/(barangay-side)/dashboard/ReportsModule/page.tsx
@@ -3942,7 +3942,7 @@ const generateDepartmentalReport = async (
       const respondentAddress = respondent.address ?? "";
 
       let remarks = "";
-      if (["CFA", "Settled", "settled", "archived", "pending"].includes(report.status || "")) {
+      if (["CFA", "Settled", "settled", "archived", "pending", "Refer to Government Agency", "Dismissed", "dismissed"].includes(report.status || "")) {
         const numHearings = report.hearing ?? 0;
         let foundRemark = false;
 
@@ -4229,7 +4229,10 @@ const generateDepartmentalReport = async (
         report.status === "Settled" || 
         report.status === "settled" || 
         report.status === "archived" ||
-        report.status === "pending") {
+        report.status === "pending" ||
+        report.status === "Refer to Government Agency" ||
+        report.status === "dismissed" ||
+        report.status === "Dismissed") {
     
       const numHearings = report.hearing ?? 0;
     
@@ -4508,7 +4511,10 @@ const generateDepartmentalReport = async (
         report.status === "Settled" || 
         report.status === "settled" || 
         report.status === "archived" ||
-        report.status === "pending") {
+        report.status === "pending" ||
+        report.status === "Refer to Government Agency" ||
+        report.status === "dismissed" ||
+        report.status === "Dismissed")  {
     
       const numHearings = report.hearing ?? 0;
     
@@ -4750,6 +4756,10 @@ const generateDepartmentalReport = async (
           department: dept,
           pending: filtered.filter((i) => i.status === "pending").length,
           inprogress: filtered.filter((i) => i.status === "In - Progress").length,
+          refer: filtered.filter((i) => i.status === "Refer to Government Agency").length,
+          dismissed: filtered.filter((i) =>
+            i.status === "dismissed" || i.status === "Dismissed"
+          ).length,
           settled: filtered.filter((i) =>
             dept === "Online"
               ? i.status === "Settled"
@@ -4786,6 +4796,9 @@ const generateDepartmentalReport = async (
         row.getCell(4).value = item.settled;
         row.getCell(5).value = item.archived;
         row.getCell(6).value = item.cfa;
+        row.getCell(7).value = item.refer;
+        row.getCell(8).value = item.dismissed;
+        row.getCell(9).value = item.acknowledged;
   
         // Style
         for (let col = 1; col <= 6; col++) {
