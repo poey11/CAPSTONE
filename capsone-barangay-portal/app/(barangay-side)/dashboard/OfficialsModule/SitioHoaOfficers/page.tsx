ae3b0d6fcab2a654a100ea282c219e6594a85150
poey11 <mocpayao15@gmail.com>
2025-09-26 04:56:42 +0800

    refactor: update officials data handling and improve user interface for adding new officials

--- capsone-barangay-portal/app/(barangay-side)/dashboard/OfficialsModule/SitioHoaOfficers/page.tsx
+++ capsone-barangay-portal/app/(barangay-side)/dashboard/OfficialsModule/SitioHoaOfficers/page.tsx
@@ -276,7 +276,7 @@ export default function SitioHoaOfficersModule() {
 
     <main className="officiers-main-container">
       <div className="officers-section-1"> 
-        {user?.position === "Admin Staff" && (
+        {(user?.position === "Admin Staff" || user?.position === "Assistant Secretary" || user?.position === "Secretary" ) && (
           <button 
           className="add-officers-btn add-officers-animated"
           onClick={() => setShowAddOOfficerPopup(true)}
@@ -377,7 +377,7 @@ export default function SitioHoaOfficersModule() {
                         <img src="/Images/view.png" alt="View"/>
                       </button>
 
-                      {user?.position === "Admin Staff" && (
+                      {(user?.position === "Admin Staff" || user?.position === "Assistant Secretary" || user?.position === "Secretary" ) && (
                         <>
                           <button 
                           className="brgy-official-action-edit"
@@ -520,15 +520,13 @@ export default function SitioHoaOfficersModule() {
                                 <>
                                   
                                   <option value="Sitio President">Sitio President</option>
-                                  <option value="Sitio Officer">Sitio Officer</option>
                                 </>
                               ):(
                                 <>
                                   <option value="Association President">Association President</option>
-                                  <option value="Association Officer">Association Officer</option>
                                 </>
                               )}
-                              <option value="Others">Others</option>
+                              {/* <option value="Others">Others</option> */}
                               {/* not sure if pwede may ibang position*/}
                             </select>
                           </div>
--- capsone-barangay-portal/app/(resident-side)/OfficialsPage/page.tsx
+++ capsone-barangay-portal/app/(resident-side)/OfficialsPage/page.tsx
@@ -47,14 +47,12 @@ export default function Official() {
 };
 
   useEffect(() => {
-    const docRef = collection(db, "BarangayUsers");
+    const docRef = collection(db, "DisplayedOfficials");
     const unsubscribe = onSnapshot(docRef, (snapshot) => {
           
       const data: Official[] = snapshot.docs.map((doc) => ({
         id: doc.id,
-        name: [doc.data().firstName, doc.data().middleName, doc.data().lastName]
-          .filter(Boolean) // removes null, undefined, and empty strings
-          .join(" "),
+        name: doc.data().name,
         position: getPositionLabel(doc.data().position, doc.data().department),
         term: doc.data().term || "N/A",
         contact: doc.data().phone,
