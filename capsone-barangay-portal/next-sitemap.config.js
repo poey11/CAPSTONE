497b7361783e82c256db3190bf4e37d4f2ebae2d
Gujetamaa <jerico.ibanez18@gmail.com>
2025-10-23 13:54:54 +0800

    SEO for google search engine

---
+++ capsone-barangay-portal/next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://barangayfairview.site', // 👈 change this to your actual domain
+  generateRobotsTxt: true, // Generates robots.txt as well
+  sitemapSize: 5000,
+};
